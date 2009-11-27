92b68d875d10b2befaa287e3e4be230b59c500d2
Tobiasschug <Tobiasschug@65f19cac-d4eb-11de-9528-add7e07fdcdc>
2009-11-27 11:47:40 +0000

    File

---
+++ 2O2P/src/oo2cad/unzip/FileCreator.java
@@ -0,0 +1,12 @@
+package oo2cad.unzip;
+
+import java.io.*;
+
+public class FileCreator 
+{
+	public File returnFileObj(String source)
+	{
+		File ooXmlContent = new File(source);
+		return ooXmlContent;
+	}
+}
