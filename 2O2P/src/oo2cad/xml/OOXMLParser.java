58ca698c9f48d8aef89a0a67b91d82808cb8fc04
manu.ahrens <manu.ahrens@65f19cac-d4eb-11de-9528-add7e07fdcdc>
2009-11-27 11:31:09 +0000

    name geändert zu OOXMLParser

---
+++ 2O2P/src/oo2cad/xml/OOXMLParser.java
@@ -0,0 +1,35 @@
+package oo2cad.xml;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.SAXException;
+
+public class OOXMLParser {
+	
+	public void parseFile(File file) {
+
+		//get a factory
+		SAXParserFactory spf = SAXParserFactory.newInstance();
+		try {
+
+			//get a new instance of parser
+			SAXParser sp = spf.newSAXParser();
+
+			//parse the file and also register this class for call backs
+			sp.parse(file, new XMLHandler());
+
+		}catch(SAXException se) {
+			se.printStackTrace();
+		}catch(ParserConfigurationException pce) {
+			pce.printStackTrace();
+		}catch (IOException ie) {
+			ie.printStackTrace();
+		}
+	}
+
+}
