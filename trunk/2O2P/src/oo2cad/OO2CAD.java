d76192875e5513ac5dd5a862a410884e8d3ef491
Tobiasschug <Tobiasschug@65f19cac-d4eb-11de-9528-add7e07fdcdc>
2009-11-27 11:30:16 +0000

    Unzip work

--- trunk/2O2P/src/oo2cad/OO2CAD.java
+++ trunk/2O2P/src/oo2cad/OO2CAD.java
@@ -1,11 +1,18 @@
 package oo2cad;
 
+import oo2cad.unzip.Unzip;
+
 public class OO2CAD {
 
 	/**
 	 * Initiale Klasse um das Programm zu starten. Über <code>args</code> wird die *.odg-Datei übergeben.
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
+		Unzip uz = new Unzip();
+		String datei = "h:\\openoffice.odg";
+		String destxml = "h:\\content.xml";
+		uz.fileExtract(datei,destxml);
 		
 		
 	}
