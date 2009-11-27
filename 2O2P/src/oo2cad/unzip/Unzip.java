de0dc2c18f29275df0b672b8dd36e7bce8bb3621
Tobiasschug <Tobiasschug@65f19cac-d4eb-11de-9528-add7e07fdcdc>
2009-11-27 11:30:16 +0000

    Unzip work

--- 2O2P/src/oo2cad/unzip/Unzip.java
+++ 2O2P/src/oo2cad/unzip/Unzip.java
@@ -1,5 +1,54 @@
+/*
+ * Unzip 
+ *
+ */
 package oo2cad.unzip;
 
-public class Unzip {
+import java.io.*;
+import java.util.*;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
+public class Unzip 
+{
+	//Zip Buffer
+	private static final byte[] buffer = new byte[ 0xFFFF ]; 
+	
+	public void fileExtract(String source, String dest)
+	{
+		int fileLength = 0;
+		
+		try
+		{
+			ZipFile ooZipFile = new ZipFile(source);
+			Enumeration<? extends ZipEntry> ooZipEntrys = ooZipFile.entries();
+			
+			//Datei in ausgabe schreiben
+			File ooXmlContent = new File(dest);
+			InputStream ooXmlContentIs = null;
+			FileOutputStream ooXmlContentFo = new FileOutputStream(ooXmlContent);
+
+			while(ooZipEntrys.hasMoreElements())
+			{
+				ZipEntry ooZipEntryInFile = ooZipEntrys.nextElement();
+				
+				//Content xml entpacken
+				if(ooZipEntryInFile.getName().equals("content.xml"))
+				{
+					//Content xml in Filestream schreiben
+					ooXmlContentIs = ooZipFile.getInputStream(ooZipEntryInFile);
+				}
+			}
+			
+			//Input Sream zurück in Datei schreiben
+			while ((fileLength = ooXmlContentIs.read(buffer)) != -1)
+			{
+				ooXmlContentFo.write(buffer, 0, fileLength);
+			}
+		}
+		catch (IOException e) 
+		{
+			System.out.println("Entpacken fehlgeschlagen ... " + e.getMessage());
+		}		
+	}
 }
---
+++ 2O2P/src/oo2cad/util/FileCreator.java
@@ -0,0 +1,12 @@
+package oo2cad.util;
+
+import java.io.*;
+
+public class FileCreator 
+{
+	public void returnFileObj(String source)
+	{
+		File ooXmlContent = null;
+		//return File;
+	}
+}
