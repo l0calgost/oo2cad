157ef8b6cca608eac6cd81d312a8975f7f703656
manu.ahrens <manu.ahrens@65f19cac-d4eb-11de-9528-add7e07fdcdc>
2009-11-27 10:39:27 +0000

    Klasse XMLParser erstellt

--- trunk/2O2P/src/oo2cad/OO2CAD.java
+++ trunk/2O2P/src/oo2cad/OO2CAD.java
@@ -3,9 +3,10 @@ package oo2cad;
 public class OO2CAD {
 
 	/**
-	 * @param args
+	 * Initiale Klasse um das Programm zu starten. Über <code>args</code> wird die *.odg-Datei übergeben.
 	 */
 	public static void main(String[] args) {
+		
 		
 	}
 
