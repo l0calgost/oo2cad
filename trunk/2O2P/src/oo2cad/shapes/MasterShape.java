274bbfa9d52312341bad3593ae5b1cb4007bba4a
manu.ahrens <manu.ahrens@65f19cac-d4eb-11de-9528-add7e07fdcdc>
2009-11-27 10:50:37 +0000

    MasterShape hinzugefügt

---
+++ trunk/2O2P/src/oo2cad/shapes/MasterShape.java
@@ -0,0 +1,12 @@
+package oo2cad.shapes;
+
+/**
+ * Diese Klasse ist das MasterShape.
+ * Alle Shape-Klassen müssen von dieser Klasse erben.
+ * 
+ * @author ahrensm
+ *
+ */
+public class MasterShape {
+
+}
