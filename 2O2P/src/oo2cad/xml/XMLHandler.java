b417d4b583bac35a94826a568be2a2c92938a88a
manu.ahrens <manu.ahrens@65f19cac-d4eb-11de-9528-add7e07fdcdc>
2009-11-30 12:27:32 +0000

    XMLHandler optimiert

--- 2O2P/src/oo2cad/xml/XMLHandler.java
+++ 2O2P/src/oo2cad/xml/XMLHandler.java
@@ -1,5 +1,7 @@
 package oo2cad.xml;
 
+import oo2cad.config.Config;
+
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
@@ -9,31 +11,25 @@ public class XMLHandler extends DefaultHandler{
 	@Override
 	public void startElement(String uri, String localName, String name,
 			Attributes attributes) throws SAXException {
-
+		/*
 		if(uri != "")
 		System.out.println(uri);
 		
 		if(localName != "")
 		System.out.println(localName);
-		
-		if(name != "")
-		System.out.println(name);
-		
-		for (int i = 0; i < attributes.getLength(); i++) {
+		*/
+		if(name.equals(Config.XML_TAG_RECT)) {
+			System.out.println(name);
 			
-			System.out.println(attributes.getQName(i) + " = " + attributes.getValue(i));			
+			for (int i = 0; i < attributes.getLength(); i++) {
+				
+				System.out.println(attributes.getQName(i) + " = " + attributes.getValue(i));
+			}
 		}
+		
+		
+		
+		
 	}
 	
-	@Override
-	public void characters(char[] ch, int start, int length)
-			throws SAXException {
-
-		for (int i = start; i < start+length; i++) {
-			System.out.print(ch[i]);
-		}
-		
-		
-	}
-
 }
