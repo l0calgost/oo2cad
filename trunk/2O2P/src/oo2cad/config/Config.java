d998f273da20eb6a4e3e1ae4174e92ccb96dda0e
manu.ahrens <manu.ahrens@65f19cac-d4eb-11de-9528-add7e07fdcdc>
2009-11-30 12:03:53 +0000

    Config-Klasse hinzu

--- trunk/2O2P/src/oo2cad/config/Config.java
+++ trunk/2O2P/src/oo2cad/config/Config.java
@@ -1,8 +1,43 @@
-/*
- * Config einlesen
- */
+
 package oo2cad.config;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * Config einlesen
+ */
 public class Config {
 
+	public static String XML_FILE_NAME;
+	public static String XML_TAG_PAGE;
+	public static String XML_TAG_RECT;
+	
+	public Config()
+	{
+		readConfigs();
+	}
+	
+	private void readConfigs() {
+		
+		Properties configs = new Properties();
+		
+		InputStream stream = Config.class.getResourceAsStream("config.properties");
+		
+		try {
+			configs.load(stream);
+			
+			stream.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		XML_FILE_NAME = configs.getProperty("xmlFileName");
+		XML_TAG_PAGE = configs.getProperty("xmlTagPage");
+		XML_TAG_RECT = configs.getProperty("rectangle");
+	}
+	
+	
 }
