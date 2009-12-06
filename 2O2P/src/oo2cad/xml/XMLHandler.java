28153a3c1a35dc7bec84a7dae9038bb34d45b05e
manu.ahrens <manu.ahrens@65f19cac-d4eb-11de-9528-add7e07fdcdc>
2009-12-06 16:26:00 +0000

    shape-objekte über reflection erzeugen

--- 2O2P/src/oo2cad/xml/XMLHandler.java
+++ 2O2P/src/oo2cad/xml/XMLHandler.java
@@ -1,84 +1,132 @@
 package oo2cad.xml;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.Vector;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import oo2cad.config.Config;
-import oo2cad.shapes.Rectangle;
 import oo2cad.shapes.Shape;
 
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
-public class XMLHandler extends DefaultHandler{
+public class XMLHandler extends DefaultHandler {
 
+	//Instanzvariablen
 	private Config config;
+	private Vector<Shape> shapeList = new Vector<Shape>();
 	
-	public XMLHandler(Config config)
-	{
+	//Konstruktor
+	public XMLHandler(Config config) {
 		this.config = config;
 	}
+
 	@Override
 	/**
-	 * Holt die Attribute aus dem entpsrechenden XML-Tag. 
-	 * Über das jeweilige XML-TAG
+	 * Holt die Attribute aus dem entpsrechenden XML-Tag über das jeweilige
+	 * XML-TAG
+	 * 
 	 * @see org.xml.sax.helpers.DefaultHandler.startElement
 	 */
 	public void startElement(String uri, String localName, String name,
 			Attributes attributes) throws SAXException {
-		
-		/*
-		if(uri != "")
-		System.out.println(uri);
-		
-		if(localName != "")
-		System.out.println(localName);
-		*/
-		try {
-			String newName = name.replace("draw:", "");
-			String penis = config.getConfigs().getProperty(newName);
-			
-			Class shapeObject = Class.forName("oo2cad.shapes."+penis);
-			int i = 0;
-			try {
-				Shape shape = (Shape) shapeObject.newInstance();
-			} catch (InstantiationException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IllegalAccessException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+
+		name = name.replace("draw:", "");
+
+		Shape shape = createShapeByName(name, attributes);
+
+		if (shape != null) {
+			shapeList.add(shape);
 		}
-		if(name.equals(Config.XML_TAG_RECT)) {
-			System.out.println(name);
-			getAttributesFloatValue(attributes.getValue(Config.XML_TAG_HEIGHT));
-			Rectangle rect = new Rectangle();
-		}		
 	}
-	
+
 	/**
-	 * Wir benötigt um aus den String-Parametern der xml den entpsprechenden
+	 * Wird benötigt um aus den String-Parametern der xml den entpsprechenden
 	 * Float-Wert zu ermitteln
-	 * @param attribute der Attribut String
+	 * 
+	 * @param attribute
+	 *            der Attribut String
 	 * @return der entpsrechende Float-Wert
 	 */
-	private float getAttributesFloatValue(String attribute)
-	{
+	private float getAttributesFloatValue(String attribute) {
 		float attributeValue = 0;
-		
-		Pattern pattern = Pattern.compile("[0-9]+[.]?[0-9]+?");
-		
-		Matcher matcher = pattern.matcher(attribute);
-		
-		while(matcher.find()){
-			System.out.println(matcher.group());
+
+		Pattern pattern = Pattern.compile("[0-9]+[.]?[0-9]?+");
+
+		if (attribute != null) {
+			Matcher matcher = pattern.matcher(attribute);
+
+			if (matcher.find()) {
+				attributeValue = Float.valueOf(matcher.group());
+			}
 		}
-		
 		return attributeValue;
+	}
+
+	/**
+	 * gibt die Liste der Shape-Objekte zurück, die aus der XML gelesen wurden.
+	 * 
+	 * @return die Shape-Objekt-Liste
+	 */
+	public Vector<Shape> getShapeList() {
+		return shapeList;
+	}
+
+	/**
+	 * Erstellt aus dem übergebenen Namen und den Attributen per Reflection das
+	 * entsprechende Shape-Objekt.
+	 * 
+	 * @param name
+	 *            der Name des XML-Tags
+	 * @param attributes
+	 *            die Attribute des XML-Tags
+	 * @return das mit Attributen versorgte Shape-Objekt
+	 */
+	private Shape createShapeByName(String name, Attributes attributes) {
+		Shape shapeObject = null;
+
+		Class shapeObjectClass;
+		try {
+			shapeObjectClass = Class.forName("oo2cad.shapes."
+					+ config.getConfigs().getProperty(name));
+
+			try {
+				shapeObject = (Shape) shapeObjectClass.newInstance();
+
+				try {
+					shapeObjectClass.getMethod("setName", String.class).invoke(shapeObject, attributes.getValue(0));
+					
+					shapeObjectClass.getMethod("setWidth", float.class).invoke(shapeObject,	getAttributesFloatValue(attributes.getValue(3)));
+					
+					shapeObjectClass.getMethod("setHeight", float.class).invoke(shapeObject, getAttributesFloatValue(attributes.getValue(4)));
+										
+					shapeObjectClass.getMethod("setX", float.class).invoke(shapeObject,	getAttributesFloatValue(attributes.getValue(5)));
+
+					shapeObjectClass.getMethod("setY", float.class).invoke(shapeObject,	getAttributesFloatValue(attributes.getValue(6)));
+					
+				} catch (IllegalArgumentException e) {
+					e.printStackTrace();
+				} catch (SecurityException e) {
+					e.printStackTrace();
+				} catch (IllegalAccessException e) {
+					e.printStackTrace();
+				} catch (InvocationTargetException e) {
+					e.printStackTrace();
+				} catch (NoSuchMethodException e) {
+					e.printStackTrace();
+				}
+			} catch (InstantiationException e1) {
+				e1.printStackTrace();
+			} catch (IllegalAccessException e1) {
+				e1.printStackTrace();
+			}
+
+		} catch (ClassNotFoundException e1) {
+
+		}
+
+		return shapeObject;
 	}
 }
