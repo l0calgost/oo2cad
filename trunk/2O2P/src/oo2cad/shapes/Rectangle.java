7aaba854ddf14bc4553de43853fdae4ad0d97485
manu.ahrens <manu.ahrens@65f19cac-d4eb-11de-9528-add7e07fdcdc>
2009-12-10 07:49:51 +0000

    Konstruktor mit Parametern raus

--- trunk/2O2P/src/oo2cad/shapes/Rectangle.java
+++ trunk/2O2P/src/oo2cad/shapes/Rectangle.java
@@ -2,17 +2,4 @@ package oo2cad.shapes;
 
 public class Rectangle extends Shape{
 
-	public Rectangle()
-	{
-		
-	}
-	
-	public Rectangle(int x, int y, int height, int width, String name) {
-		super.setX(x);
-		super.setY(y);
-		super.setHeight(height);
-		super.setWidth(width);
-		super.setName(name);
-	}
-	
 }
