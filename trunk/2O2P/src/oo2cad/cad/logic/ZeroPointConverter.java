4a9366038f17b9ecb9c2c9abdae0d643d103c2ab
manu.ahrens <manu.ahrens@65f19cac-d4eb-11de-9528-add7e07fdcdc>
2009-12-11 11:41:55 +0000

    -

---
+++ trunk/2O2P/src/oo2cad/cad/logic/ZeroPointConverter.java
@@ -0,0 +1,18 @@
+package oo2cad.cad.logic;
+
+import java.util.Vector;
+
+import oo2cad.shapes.Shape;
+
+/**
+ * Wird benötigt um die Koordinaten von OO (Null-Punkt links oben)
+ * zu CAD (Null-Punkt links unten) zu konvertieren
+ *
+ */
+public class ZeroPointConverter {
+
+	public void convertValues(Vector<Shape> shapeList)
+	{
+		
+	}
+}
