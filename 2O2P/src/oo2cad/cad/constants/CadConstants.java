254321124c0ab94498cce1e5d2a63720c577dfe6
manu.ahrens <manu.ahrens@65f19cac-d4eb-11de-9528-add7e07fdcdc>
2009-12-14 15:49:13 +0000

    Konstantenklasse für CAD-Code

---
+++ 2O2P/src/oo2cad/cad/constants/CadConstants.java
@@ -0,0 +1,19 @@
+package oo2cad.cad.constants;
+
+/**
+ * Konstanten-Klasse für CAD-Befehle
+ *
+ */
+public class CadConstants {
+
+	public static final String OBJECT_START = "os";
+	public static final String OBJECT_END = "oe";
+	public static final String OBJECTBOX = "ob";
+	public static final String COLOR = "co";
+	public static final String MOVE_ABSOLUT = "ma";
+	public static final String DRAW_ABSOLUT = "da";
+	public static final String DRAW_ELLIPSE = "el";
+	public static final String DRAW_ELLIPSE_PART = "es";
+	
+	public static final String RETURN = "\n";
+}
