caceef8d9de4e1a0630871deac367b3327439cb8
manu.ahrens <manu.ahrens@65f19cac-d4eb-11de-9528-add7e07fdcdc>
2009-12-16 09:31:27 +0000

    Nullpunktkonvertierer vor cad-code-erstellung gepackt

--- 2O2P/src/oo2cad/cad/logic/CadHandler.java
+++ 2O2P/src/oo2cad/cad/logic/CadHandler.java
@@ -3,35 +3,42 @@ package oo2cad.cad.logic;
 import java.util.Vector;
 
 import oo2cad.cad.objects.ObjectBox;
+import oo2cad.shapes.Rectangle;
 import oo2cad.shapes.Shape;
 
-public class CadHandler 
-{
+public class CadHandler {
 	private Vector<Shape> shapeList;
-	
-	public CadHandler(Vector<Shape> shapeList)
-	{
-		this.shapeList = shapeList;		
+
+	public CadHandler(Vector<Shape> shapeList) {
+		this.shapeList = shapeList;
 	}
-	
+
 	//
-	public void createCadCode()
-	{
+	public void createCadCode() {
+		
+		//Konvertierung der Koordinaten von OpenOffice (Nullpunkt links oben) zu CAD (Nullpunkt links unten)
+		new ZeroPointConverter().convertValues(shapeList);
+		
 		/*
 		 * ShapeObjecte auslesen und x,y Min,Max bestimmen für ObjectBox
 		 */
 		
 		ObjectBoxValueGetter obvg = new ObjectBoxValueGetter(shapeList);
 		obvg.objectboxValuesMaxMin();
+
+//		for (Shape shape : shapeList) {
+//						
+//			if(shape instanceof Rectangle)
+//			{
+//				System.out.println(shape.getClass().getSimpleName());
+//			}
+//		}
 		//ObjetBox mit Werten befüllen
 		ObjectBox objectBox = new ObjectBox(obvg.getxMin(), obvg.getxMax(), obvg.getyMin(), obvg.getyMax());
-		// Zero Punkt neu setzen (muss nach objectBox passieren, da sonst teile der Shapes abgeschnitten werden)
-		new ZeroPointConverter().convertValues(shapeList);
 		
-		
+		//TODO
 	}
 
-	
 	public Vector<Shape> getShapeList() {
 		return shapeList;
 	}
