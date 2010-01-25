b963f31738b99ecf9603976f745d95b752faf63e
manu.ahrens <manu.ahrens@65f19cac-d4eb-11de-9528-add7e07fdcdc>
2010-01-25 12:15:15 +0000

    log4j eingefügt

--- trunk/2O2P/src/oo2cad/OO2CAD.java
+++ trunk/2O2P/src/oo2cad/OO2CAD.java
@@ -4,50 +4,59 @@ import java.io.File;
 import java.util.Vector;
 
 import oo2cad.cad.logic.CadHandler;
-import oo2cad.cad.objects.ObjectBox;
 import oo2cad.config.Config;
 import oo2cad.shapes.Shape;
-import oo2cad.unzip.Unzip;
 import oo2cad.xml.OOXMLParser;
+
+import org.apache.log4j.Logger;
 
 public class OO2CAD {
 
+	static Logger log = Logger.getLogger(OO2CAD.class);
+	
 	/**
-	 * Initiale Klasse um das Programm zu starten. Über <code>args</code> wird die *.odg-Datei übergeben.
+	 * Initiale Klasse um das Programm zu starten. ï¿½ber <code>args</code> wird
+	 * die *.odg-Datei ï¿½bergeben.
 	 */
-	public static void main(String[] args) 
-	{
-		//Configurations aus der config.properties auslesen
+	public static void main(String[] args) {
+		
+		//DOMConfigurator.configureAndWatch("/config/log4j.xml");
+		
+		// Configurations aus der config.properties auslesen
 		Config config = new Config();
-		
-		//aus der *.odg-Datei die content.xml holen
+
 		String datei = "h:\\openoffice.odg";
-		File ooXmlContent = new Unzip().extractFile(datei, config.getConfigs().getProperty("xmlFileName"));
+		log.info("OpenOffice datei eingelesen! Pfad: " + datei);
+		
+		// aus der *.odg-Datei die content.xml holen
 
-		try 
-		{
-			//File ooXmlContent = uz.extractFile(datei,Config.XML_FILE_NAME);
-			
-			//File an Parser übergeben
+		//Unzip uz = new Unzip();
+		File ooXmlContent = new File(config.getConfigs().getProperty("xmlFileName"));//uz.extractFile(datei, config.getConfigs().getProperty("xmlFileName"));
+
+		try {
+			// File ooXmlContent = uz.extractFile(datei,Config.XML_FILE_NAME);
+
+			// File an Parser ï¿½bergeben
 			OOXMLParser parser = new OOXMLParser(config);
-			parser.parseFile(ooXmlContent/*new File(config.getConfigs().getProperty("xmlFileName"))*/);
-			
-			//CadHandler managed alle Convertierungsvorgänge in CAD
-			CadHandler cadHandler = new CadHandler(parser.getXmlHandler().getShapeList());
+
+			parser.parseFile(ooXmlContent);
+
+			parser.parseFile(ooXmlContent);
+
+			Vector<Shape> shapeList = parser.getXmlHandler().getShapeList();
+			int i = 0;
+			i++;
+
+			// CadHandler managed alle Convertierungsvorgï¿½nge in CAD
+			CadHandler cadHandler = new CadHandler(parser.getXmlHandler()
+					.getShapeList());
 			cadHandler.createCadCode();
-			
+
 			System.out.println("");
-			
-			
-		}
-		catch(Exception e) {
-			
-		}
 
-		
+		} catch (Exception e) {
 
-		
-		
+		}
 
 	}
 
