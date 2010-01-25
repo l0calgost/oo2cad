5103aaeaa6d4bd494f3961ef3f0f09fe42cd0953
Tobiasschug <Tobiasschug@65f19cac-d4eb-11de-9528-add7e07fdcdc>
2010-01-25 12:44:55 +0000

    ShapeList

--- trunk/2O2P/src/oo2cad/cad/objects/ObjectBox.java
+++ trunk/2O2P/src/oo2cad/cad/objects/ObjectBox.java
@@ -1,18 +1,24 @@
 package oo2cad.cad.objects;
 
+import java.util.Vector;
+
+import oo2cad.shapes.Shape;
+
 public class ObjectBox 
 {
 	private float xMin;
 	private float xMax;
 	private float yMin;
 	private float yMax;
+	private Vector<Shape> shape;
 	
-	public ObjectBox(float xMin, float xMax, float yMin, float yMax)
+	public ObjectBox(float xMin, float xMax, float yMin, float yMax, Vector<Shape> shape)
 	{
 		this.xMin = xMin;
 		this.xMax = xMax;
 		this.yMin = yMin;
 		this.yMax = yMax;
+		this.shape = shape;
 	}
 	
 	public float getxMin() {
@@ -39,6 +45,12 @@ public class ObjectBox
 	public void setyMax(float yMax) {
 		this.yMax = yMax;
 	}
-	
-	
+	public Vector<Shape> getShape()
+	{
+		return shape;
+	}
+	public void setShape(Vector<Shape> shape)
+	{
+		this.shape = shape;
+	}
 }
