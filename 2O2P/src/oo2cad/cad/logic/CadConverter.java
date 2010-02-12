d16e77a5b749eb20f44395c035b791f7ecac01ce
manu.ahrens <manu.ahrens@65f19cac-d4eb-11de-9528-add7e07fdcdc>
2010-02-12 11:27:04 +0000

    Umwandlung ausgelagert

--- 2O2P/src/oo2cad/cad/logic/CadConverter.java
+++ 2O2P/src/oo2cad/cad/logic/CadConverter.java
@@ -2,6 +2,7 @@ package oo2cad.cad.logic;
 
 import java.util.Vector;
 
+import oo2cad.cad.logic.shapes.StandardGeometryConverter;
 import oo2cad.cad.objects.CadBaseObject;
 import oo2cad.cad.objects.CadLine;
 import oo2cad.shapes.Line;
@@ -19,18 +20,31 @@ public class CadConverter
 	public Vector<CadBaseObject> convertShapes(Vector<Shape> shapeList)
 	{
 		Vector<CadBaseObject> cadShapeList = new Vector<CadBaseObject>();
+		StandardGeometryConverter stageco = new StandardGeometryConverter();
 		
 		for (Shape shape : shapeList)
 		{
 			CadBaseObject[] objectArray = null;
+			
 			if (shape instanceof Line)
 			{
-				objectArray = convertLine((Line) shape);	
+				/*
+				Ist das OpenOffice-Objekt eine Line, haben die Methoden nicht
+				den gleichen namen wie die get- bzw. set-Methoden angeben.
+				Es gibt folgende Zuordung:
+					- width  = x-Wert des Startpunkts
+					- height = y-Wert des Startpunkts
+					- x	     = x-Wert des Endpunkts
+					- y      = y-Wert des Endpunkts
+				Diese Zuordnung wird gemacht, dass die Erzeugung per
+				Reflection möglichst einfach ist.
+				*/
+				objectArray = stageco.convertLine((Line) shape);	
 			}
 			
 			if (shape instanceof Rectangle)
 			{
-				objectArray =convertRectangle((Rectangle) shape);
+				objectArray = stageco.convertRectangle((Rectangle) shape);
 			}
 			
 			if (objectArray != null)
@@ -40,44 +54,11 @@ public class CadConverter
 					cadShapeList.add(objectArray[i]);
 				}
 			}
-			
 		}
 		
 		return cadShapeList;
 	}
+		
 	
-	
-	private CadBaseObject[] convertLine(Line line)
-	{
-		/*
-		Ist das OpenOffice-Objekt eine Line, haben die Methoden nicht
-		den gleichen namen wie die get- bzw. set-Methoden angeben.
-		Es gibt folgende Zuordung:
-			- width  = x-Wert des Startpunkts
-			- height = y-Wert des Startpunkts
-			- x	     = x-Wert des Endpunkts
-			- y      = y-Wert des Endpunkts
-		Diese Zuordnung wird gemacht, dass die Erzeugung per
-		Reflection möglichst einfach ist.
-		*/
-		CadBaseObject[] lineArray = new CadBaseObject[1];
-		
-		CadLine cadLine = new CadLine(line.getWidth(), line.getHeight(), line.getX(), line.getY());
-		lineArray[0] = cadLine;
-		
-		return lineArray;
-	}
-	
-	private CadBaseObject[] convertRectangle(Rectangle rect)
-	{
-		CadBaseObject[] rectArray = new CadBaseObject[4];
-		
-		rectArray[0] = new CadLine(rect.getX(), rect.getY(), rect.getX() + rect.getWidth(),	rect.getY() + rect.getWidth());
-		rectArray[1] = new CadLine(rect.getX() + rect.getWidth(), rect.getY() + rect.getWidth(), rect.getX() + rect.getWidth() + rect.getHeight(), rect.getY() + rect.getWidth() + rect.getHeight());
-		rectArray[2] = new CadLine(rect.getX() + rect.getHeight(), rect.getY() + rect.getHeight(), rect.getX() + rect.getHeight(), rect.getY() + rect.getHeight());
-		rectArray[3] = new CadLine(rect.getX() + rect.getHeight(), rect.getY() + rect.getHeight(), rect.getX(), rect.getY());
-		
-		return rectArray;
-	}
 
 }
