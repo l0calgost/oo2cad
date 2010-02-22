4340528eaf1402ce11165b0b859284a011b00422
manu.ahrens <manu.ahrens@65f19cac-d4eb-11de-9528-add7e07fdcdc>
2010-02-22 16:23:12 +0000

    Vererbungshirarchie angepasst

--- trunk/2O2P/src/oo2cad/shapes/Shape.java
+++ trunk/2O2P/src/oo2cad/shapes/Shape.java
@@ -2,49 +2,20 @@ package oo2cad.shapes;
 
 /**
  * Diese Klasse ist das MasterShape.
- * Alle Shape-Klassen müssen von dieser Klasse erben.
+ * Diese Klasse beerbt die Klassen AdvancedShape
+ * und SimpleShape
  * 
  * @author ahrensm
  *
  */
 public class Shape {
 
-	private float x;
-	private float y;
-	private float height;
-	private float width;
 	private String name;
 	
-	public float getX() {
-		return x;
-	}
-	public void setX(float x) {
-		this.x = x;
-	}
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
-	}
-	public float getY() {
-		return y;
-	}
-	public void setY(float y) {
-		this.y = y;
-	}
-	public float getHeight() {
-		return height;
-	}
-	public void setHeight(float height) {
-		this.height = height;
-	}
-	public float getWidth() {
-		return width;
-	}
-	public void setWidth(float width) {
-		this.width = width;
-	}
-	
-	
+	}	
 }
