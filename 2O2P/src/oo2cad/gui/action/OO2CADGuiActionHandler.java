d0e791145a4a647df97ada0a8c66be52efb6b5ea
manu.ahrens <manu.ahrens@65f19cac-d4eb-11de-9528-add7e07fdcdc>
2010-03-02 08:48:42 +0000

    Klasse für die GUI

---
+++ 2O2P/src/oo2cad/gui/action/OO2CADGuiActionHandler.java
@@ -0,0 +1,14 @@
+package oo2cad.gui.action;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class OO2CADGuiActionHandler implements ActionListener
+{
+
+	public void actionPerformed(ActionEvent arg0)
+	{
+				
+	}
+
+}
