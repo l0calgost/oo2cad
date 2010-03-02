a767102f2fe40e46228d2afa58dd4fd03a78077e
manu.ahrens <manu.ahrens@65f19cac-d4eb-11de-9528-add7e07fdcdc>
2010-03-02 10:08:00 +0000

    gui-komponenten hinzu

--- 2O2P/src/oo2cad/gui/OO2CADGui.java
+++ 2O2P/src/oo2cad/gui/OO2CADGui.java
@@ -1,8 +1,31 @@
 package oo2cad.gui;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class OO2CADGui extends JFrame
 {
+	/*
+	 * Komponenten
+	 */
+	private JButton closeButton;
+	private JButton convertButton;
 
+	private JButton sourceButton;
+	private JButton destButton;
+	
+	private JTextField sourcePathTextField;
+	private JTextField destPathTextField;
+	
+	private JTextField measureXTextField;
+	private JTextField measureYTextField;
+	
+	private JTextField offSetWidthTextField;
+	private JTextField offSetHeightTextField;
+	
+	private JLabel measureLabel;
+	private JLabel offSetLabel;
+	
 }
