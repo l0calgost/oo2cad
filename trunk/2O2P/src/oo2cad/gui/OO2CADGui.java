1832279aa8d4a72cee34850f7736c38548d725d6
Tobiasschug <Tobiasschug@65f19cac-d4eb-11de-9528-add7e07fdcdc>
2010-03-12 11:48:25 +0000

    GUI

--- trunk/2O2P/src/oo2cad/gui/OO2CADGui.java
+++ trunk/2O2P/src/oo2cad/gui/OO2CADGui.java
@@ -1,9 +1,17 @@
 package oo2cad.gui;
 
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 public class OO2CADGui extends JFrame
 {
@@ -25,7 +33,113 @@ public class OO2CADGui extends JFrame
 	private JTextField offSetWidthTextField;
 	private JTextField offSetHeightTextField;
 	
-	private JLabel measureLabel;
-	private JLabel offSetLabel;
+	private JLabel measureLabelX;
+	private JLabel measureLabelY;
 	
+	private JLabel offSetLabelHeight;
+	private JLabel offSetLabelWidth;
+
+	
+	
+	public void Window()
+	{
+		//Look and Feel
+		try
+		{
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedLookAndFeelException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		//JFrame
+		JFrame jf = new JFrame();
+		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jf.setLayout(new GridLayout(0,2,6,3));		
+		
+		//Button Labeln		
+		sourceButton = new JButton("Select");
+		destButton = new JButton("Save");
+		
+		measureLabelX = new JLabel("Verhältniss");
+		measureLabelY = new JLabel("Verhältniss");
+		
+		offSetLabelHeight = new JLabel("Offset Höhe:(X) ");
+		offSetLabelWidth = new JLabel("Offset Width:(Y) ");
+		
+		convertButton = new JButton("Start...");
+		closeButton = new JButton("Close");
+		
+		//TextFelder erzeugen
+		sourcePathTextField = new JTextField();
+		destPathTextField = new JTextField();
+		
+		measureXTextField = new JTextField();
+		measureYTextField = new JTextField();
+		
+		offSetHeightTextField = new JTextField();
+		offSetWidthTextField = new JTextField();
+		
+		//OpenFile Dialog
+		JFileChooser jfSource = new JFileChooser();
+		jfSource.setDialogType(JFileChooser.OPEN_DIALOG);
+		jfSource.setFileSelectionMode(JFileChooser.FILES_ONLY);
+		File file = new File("C:\\");
+		jfSource.setCurrentDirectory(file);
+		
+		JFileChooser jfDest = new JFileChooser();
+		jfDest.setDialogType(JFileChooser.SAVE_DIALOG);
+		
+		//Button Click Funktionen
+		ActionListener alSourceButton = new ActionListener()
+		{			
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+								
+			}
+		};
+		sourceButton.addActionListener(alSourceButton);
+		
+		
+		//Source Pfad
+		jf.add(sourceButton);
+		jf.add(sourcePathTextField);		
+		//Dest Pfad
+		jf.add(destButton);
+		jf.add(destPathTextField);
+		
+		
+		// Größenverhältnis
+		jf.add(measureLabelX);
+		jf.add(measureXTextField);
+		jf.add(measureLabelY);
+		jf.add(measureYTextField);
+		
+		//Offset
+		jf.add(offSetLabelHeight);
+		jf.add(offSetHeightTextField);
+		jf.add(offSetLabelWidth);
+		jf.add(offSetWidthTextField);		
+		
+		//Close und Convert Button
+		jf.add(convertButton);
+		jf.add(closeButton);
+		
+		jf.pack();
+		jf.setVisible(true);
+	}	
 }
