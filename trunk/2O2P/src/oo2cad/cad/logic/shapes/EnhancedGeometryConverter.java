912bdce3e5805660a989fbb0b12dc43cb82711ea
manu.ahrens <manu.ahrens@65f19cac-d4eb-11de-9528-add7e07fdcdc>
2010-03-30 09:08:49 +0000

    convertCircle-Methode eingefügt

--- trunk/2O2P/src/oo2cad/cad/logic/shapes/EnhancedGeometryConverter.java
+++ trunk/2O2P/src/oo2cad/cad/logic/shapes/EnhancedGeometryConverter.java
@@ -2,7 +2,7 @@ package oo2cad.cad.logic.shapes;
 
 import oo2cad.cad.objects.CadBaseObject;
 import oo2cad.cad.objects.CadBow;
-import oo2cad.cad.objects.CadLine;
+import oo2cad.shapes.Circle;
 import oo2cad.shapes.Ellipse;
 
 public class EnhancedGeometryConverter
@@ -12,6 +12,16 @@ public class EnhancedGeometryConverter
 		CadBow[] bowArray = new CadBow[1];
 		
 		//CadBow cadBow = new CadBow(radiusX, radiusY, angleStart, angleEnd);
+		//bowArray[0] = cadBow;
+		
+		return bowArray;
+	}
+
+	public CadBaseObject[] convertCircle(Circle circle)
+	{
+		CadBow[] bowArray = new CadBow[1];
+		
+		//CadBow cadBow = new CadBow(radiusX, radiusY, 0, 360)
 		//bowArray[0] = cadBow;
 		
 		return bowArray;
